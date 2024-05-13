b8f9d527d922826cc84d31240c789ba93d3f1d66
Dennis Floer <husocl@gmail.com>
2024-05-13 15:12:16 +0200

    Added following files: config.json karten.py maumau.py rename <- MySQL.py

---
+++ connection/MySQL.py
@@ -0,0 +1,20 @@
+import mysql.connector                  #Import von der MySQL Lib
+import json                             #Import der JSON Lib
+
+# Pfad zur Konfigurationsdatei
+config_file = ".\credentials\config.json"
+
+# Funktion zum Lesen der Konfigurationsdatei
+def read_config():
+    with open(config_file) as f:
+        config = json.load(f)
+    return config
+
+# Verwendung der Funktion zum Lesen der Konfigurationsdatei
+config = read_config()
+
+# Extrahieren der Logindaten
+benutzername = config["benutzername"]
+passwort = config["passwort"]
+host = config["host"]
+datenbank = config["datenbank"]
